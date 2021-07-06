21c57412cdaa3714c7872cf36ef84d874d7b713c
Juan Carlos Galvis Rodriguez <jcgalvis6@hotmail.com>
2021-07-06 16:00:43 -0500

    Feature: Add category list

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+
+  const handleAdd = () => {
+    setCategories([...categories, "Prueba"]);
+  };
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+      <button onClick={handleAdd}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
